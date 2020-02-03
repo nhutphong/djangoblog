cca1677cfdbb84fafcdec49b310d14f5d1b09c1e
root <root@localhost.localdomain>
2020-02-03 11:14:11 +0700

    updated

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -29,6 +29,55 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
     path('contact/', contact_view),
+
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='registration/password_change.html'
+            ), 
+        name='password_change'
+        ),
+
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='registration/password_change_done.html'
+            ), 
+        name='password_change_done'
+        ),
+
+    path(
+        'password_reset/', 
+        auth_views.PasswordResetView.as_view(), 
+        name='password_reset'
+    ),
+
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_done.html'
+            ),
+        name='password_reset_done'
+        ),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'
+    ),
+
+    path(
+        'reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+            ),
+        name='password_reset_complete'
+     ),
+
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
