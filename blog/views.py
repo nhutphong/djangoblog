1a042858ef7a8c056417598b438446be3a06fcea
root <root@localhost.localdomain>
2020-02-05 08:03:17 +0700

    5-2

--- blog/views.py
+++ blog/views.py
@@ -20,31 +20,6 @@ from .forms import ArticleModelForm
 from .models import Article
 
 
-# @method_decorator(login_required, name='dispatch')
-class ArticleCreateView(LoginRequiredMixin, CreateView):
-    #yeu cau dang nhap moi run dc ArticleCreateView
-    #thua ke tu LoginRequiredMixin
-    login_url = '/accounts/login/'
-    redirect_field_name = 'next'
-
-    template_name = 'articles/article_create.html'
-    form_class = ArticleModelForm
-    queryset = Article.objects.all()  # <blog>/<modelname>_list.html
-    #success_url = '/'
-
-    def form_valid(self, form):
-        print(form.cleaned_data)
-
-        #form.instance.content = Article.content
-        form.instance.content = 'da changed from form_valid(self, form)'
-        print(form.cleaned_data)
-
-        return super().form_valid(form)
-
-    # def get_success_url(self):
-    #    return '/'
-
-
 class ArticleListView(ListView):
     # cac attrs class co nhieu methods tuong ung de thuc hien nhieu login hon
     # queryset <=> get_context_data(self)
@@ -76,12 +51,37 @@ class ArticleDetailView(DetailView):
         return get_object_or_404(Article, id=id_)
 
 
+# @method_decorator(login_required, name='dispatch')
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    #yeu cau dang nhap moi run dc ArticleCreateView
+    #thua ke tu LoginRequiredMixin
+    # login_url = '/accounts/login/'
+    # redirect_field_name = 'next'
+
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all()  # <blog>/<modelname>_list.html
+    #success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+
+        #form.instance.content = Article.content
+        form.instance.content = 'da changed from form_valid(self, form)'
+        print(form.cleaned_data)
+
+        return super().form_valid(form)
+
+    # def get_success_url(self):
+    #    return '/'
+
+
 # dung cho class base view
 # @method_decorator(login_required, name='dispatch')
 class ArticleUpdateView(LoginRequiredMixin, UpdateView):
 
-    login_url = '/accounts/login/'
-    redirect_field_name = 'next'
+    # login_url = '/accounts/login/'
+    # redirect_field_name = 'next'
 
     template_name = 'articles/article_create.html'
     form_class = ArticleModelForm
