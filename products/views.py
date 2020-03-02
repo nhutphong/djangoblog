8c2f48edff6ed165ec030b6b73cc34e08342fec4
nhutphong <nhutphong@outlook.com>
2020-03-03 06:54:48 +0700

    move3-3

--- products/views.py
+++ products/views.py
@@ -5,30 +5,6 @@ from .forms import ProductForm
 from .models import Product
 
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('products:product-list')
-    
-    context = {
-        'form': form
-    }
-    return render(request, "products/product_create.html", context)
-
-@login_required
-def product_update_view(request, id):
-    product = get_object_or_404(Product, id=id)
-    form = ProductForm(request.POST or None, instance=product)
-    if form.is_valid():
-        form.save()
-        return redirect('products:product-list')
-    context = {
-        'form': form
-    }
-    return render(request, "products/product_create.html", context)
-
-
 def product_list_view(request):
     queryset = Product.objects.all()[::-1]  # list of objects
     context = {
@@ -43,6 +19,32 @@ def product_detail_view(request, id):
         "object": product
     }
     return render(request, "products/product_detail.html", context)
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('products:product-list')
+    
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+@login_required
+def product_update_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return redirect('products:product-list')
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
 
 @login_required
 def product_delete_view(request, id):
