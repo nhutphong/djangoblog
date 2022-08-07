5b42b80fce8a7ffefede70f8e410cc85801ce413
nhutphong <nhutphong@outlook.com>
2022-08-07 08:55:32 +0700

    @admin.display

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,16 @@ from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content',)
+    #list_display = ('title', 'content',)
+    list_display = ('title', 'content_short',)
+
     prepopulated_fields = {'slug': ('title',)}  # new
+
+
+    # view column content chi 100 chars
+    @admin.display(description='content 100')
+    def content_short(self, obj):
+        return obj.content[:100] + '...'
 
 
 admin.site.register(Article, ArticleAdmin)
