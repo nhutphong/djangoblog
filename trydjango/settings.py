fba4acfaa457a4eb23b176f232861d0060b03282
nhutphong <nhutphong@outlook.com>
2022-08-08 10:44:15 +0700

    Procfile

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*h*+i7yqlh8aq4f+@+par@u!)$b3y0eyum#f4j_0t8u$uor7)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
