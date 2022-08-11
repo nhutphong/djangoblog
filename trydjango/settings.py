42eb6e0b2a29ee77a0e5f7518f2df3da2954e9b6
nhutphong <nhutphong@outlook.com>
2022-08-11 10:17:26 +0700

    make it better

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -255,3 +255,7 @@ if "CI" in os.environ:
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
