7990db5340bc2191432a793084fb5027a100a5ea
nhutphong <nhutphong@outlook.com>
2022-10-12 11:21:17 +0700

    modify

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -128,7 +128,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # folder templates/
+        # folder /app-name/templates/app-name/*.html
         'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -197,10 +197,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
